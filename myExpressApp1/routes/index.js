b8773ff55bedd5edc63c9d26f9d62dd2d63e92bb
Abhishek <abhisheksinghasr734@gmail.com>
2024-01-18 02:12:18 +0530

    C,R,D sessions

--- myExpressApp1/routes/index.js
+++ myExpressApp1/routes/index.js
@@ -34,4 +34,30 @@ router.get("/", function (req, res) {
 //   res.send(deletedUser);
 // });
 
+router.get("/adult", function (req, res) {
+  req.session.ban = true;
+  res.send("you got banned :(");
+});
+
+router.get("/checkban", function (req, res) {
+  if (req.session.ban === true) {
+    console.log(req.session);
+    res.send(`Oops! you are banned :p`);
+  } else {
+    console.log(req.session);
+    res.send("You are not banned");
+  }
+});
+
+router.get("/removeban", function (req, res) {
+  if (req.session.ban === true) {
+    req.session.destroy(function (err) {
+      if (err) throw err;
+      res.send("Your ban got removed! ;)");
+    });
+  } else {
+    res.send("You were not banned");
+  }
+});
+
 module.exports = router;
