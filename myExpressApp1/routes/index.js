7b2ec623e8a60a32e3fbd48d605e6a2ad3de1ef3
Abhishek <abhisheksinghasr734@gmail.com>
2024-01-18 01:50:42 +0530

    CRUD mongoDB

--- myExpressApp1/routes/index.js
+++ myExpressApp1/routes/index.js
@@ -6,14 +6,32 @@ router.get("/", function (req, res) {
   res.render("index");
 });
 
-router.get("/create", async function (req, res) {
-  const createdUser = await userModel.create({
-    username: "asr",
-    age: 20,
-    name: "Abhishek",
-  });
+//create user---------------------------------------------------------------
+// router.get("/create", async function (req, res) {
+//   const createdUser = await userModel.create({
+//     username: "asr",
+//     age: 20,
+//     name: "Abhishek",
+//   });
 
-  res.send(createdUser);
-});
+//   res.send(createdUser);
+// });
+
+//find user-----------------------------------------------------------------------
+// router.get("/alluser", async function (req, res) {
+//   const userData = await userModel.find({ username: "asr" });
+
+//   res.send(userData);
+// });
+
+// delete user-----------------------------------------------------------------------------------
+
+// router.get("/delete", async function (req, res) {
+//   const deletedUser = await userModel.findOneAndDelete({
+//     username: "asr",
+//   });
+
+//   res.send(deletedUser);
+// });
 
 module.exports = router;
